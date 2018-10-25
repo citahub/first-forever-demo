73a277691c5c9df54b1098d53caaeb8b6788e6c7
Keith <keithwhisper@gmail.com>
2018-10-25 14:37:02 +0800

    update: enable neuron

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-window.addEventListener('neuronWebReady', () => {
-  setTimeout(() => {
-    ReactDOM.render(<App />, document.getElementById('root'))
-  }, 10)
-})
+// window.addEventListener('neuronWebReady', () => {
+//   setTimeout(() => {
+ReactDOM.render(<App />, document.getElementById('root'))
+//   }, 10)
+// })
 registerServiceWorker()
--- src/nervos.js
+++ src/nervos.js
@@ -6,6 +6,7 @@ const config = require('./config')
 
 const nervos = Nervos(config.chain) // config.chain indicates that the address of Appchain to interact
 
+
 // NOTICE:
 // this account used to deploy contract, when build the front - end app it should be removed
 //
@@ -13,12 +14,22 @@ const nervos = Nervos(config.chain) // config.chain indicates that the address o
 // nervos.appchain.accounts.wallet.add(account) // add account to nervos
 
 // add neuron web messenger from chrome
-if (window) {
-  window.addEventListener("neuronWebReady", () => {
-    if (window.addMessenger) {
-      window.addMessenger(nervos)
-    }
-  })
+// if (window) {
+//   window.addEventListener("neuronWebReady", () => {
+//     if (window.addMessenger) {
+//       window.addMessenger(nervos)
+//     }
+//   })
+// }
+
+// add neruon provider
+if (typeof window.nervos !== 'undefined') {
+  window.nervos = Nervos(window.nervos.currentProvider)
+  window.nervos.currentProvider.setHost('localhost:1337') // set CITA node IP address and port
+} else {
+  console.log('No nervos? You should consider trying Neuron!')
+  window.nervos = Nervos(config.chain)
 }
+var nervos = window.nervos
 
 module.exports = nervos
