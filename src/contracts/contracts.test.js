17b50711d3ee4ff26083bab7eac8a90d2e2acfb2
Bao Jun <baojun.cz@gmail.com>
2019-04-29 19:15:57 +0800

    fix: deploy can not find env

--- src/contracts/contracts.test.js
+++ src/contracts/contracts.test.js
@@ -1,37 +1,37 @@
-const cita = require('../cita')
-const {
-  abi
-} = require('./compiled')
-const {
-  contractAddress
-} = require('../config')
-const transaction = require('./transaction')
+const cita = require("../cita-sdk");
+const { abi } = require("./compiled");
+const { contractAddress } = require("../config");
+const transaction = require("./transaction");
 
-const simpleStoreContract = new cita.base.Contract(abi, contractAddress) // instantiate contract
+const simpleStoreContract = new cita.base.Contract(abi, contractAddress); // instantiate contract
 
-cita.base.getBalance(cita.base.accounts.wallet[0].address).then(console.log) // check balance of account
-console.log(`Interact with contract at ${contractAddress}`)
-const time = new Date().getTime()
-const text = 'hello world at ' + time
+cita.base.getBalance(cita.base.accounts.wallet[0].address).then(console.log); // check balance of account
+console.log(`Interact with contract at ${contractAddress}`);
+const time = new Date().getTime();
+const text = "hello world at " + time;
 
 test(`Add record of (${text}, ${time})`, async () => {
-  const current = await cita.base.getBlockNumber()
-  transaction.validUntilBlock = +current + 88 // update transaction.validUntilBlock
-  const txResult = await simpleStoreContract.methods.add(text, time).send(transaction) // sendTransaction to the contract
-  const receipt = await cita.listeners.listenToTransactionReceipt(txResult.hash) // listen to the receipt
-  expect(receipt.errorMessage).toBeNull()
-}, 10000)
+  const current = await cita.base.getBlockNumber();
+  transaction.validUntilBlock = +current + 88; // update transaction.validUntilBlock
+  const txResult = await simpleStoreContract.methods
+    .add(text, time)
+    .send(transaction); // sendTransaction to the contract
+  const receipt = await cita.listeners.listenToTransactionReceipt(
+    txResult.hash
+  ); // listen to the receipt
+  expect(receipt.errorMessage).toBeNull();
+}, 10000);
 
-test(`Get record of (${text}, ${time})`, (done) => {
+test(`Get record of (${text}, ${time})`, done => {
   setTimeout(async () => {
     const list = await simpleStoreContract.methods.getList().call({
-      from: transaction.from,
-    }) // check list
+      from: transaction.from
+    }); // check list
     const msg = await simpleStoreContract.methods.get(time).call({
-      from: transaction.from,
-    }) // check message
-    expect(+list[list.length - 1]).toBe(time)
-    expect(msg).toBe(text)
-    done()
-  }, 8000)
-}, 10000)
+      from: transaction.from
+    }); // check message
+    expect(+list[list.length - 1]).toBe(time);
+    expect(msg).toBe(text);
+    done();
+  }, 8000);
+}, 10000);
