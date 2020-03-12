9245209b7f1eec59be549023d3a29affb6f3fb75
travis_joe <qiuzengyuan@rivtower.com>
2020-03-12 14:52:39 +0800

    feat: contract support image

--- src/contracts/SimpleStore.sol
+++ src/contracts/SimpleStore.sol
@@ -1,15 +1,21 @@
 pragma solidity 0.4.24;
 
+
 contract SimpleStore {
-    mapping (address => mapping (uint256 => string)) private records;
+    mapping (address => mapping (uint256 => Message)) private records;
     mapping (address => uint256[]) private categories;
-    
-    event Recorded(address _sender, string indexed _text, uint256 indexed _time);
-    
+
+    struct Message {
+        string msgType;
+        string msgContent;
+    }
+
+    event Recorded(address _sender, string indexed _text, string msgType, uint256 indexed _time);
+
     function _addToList(address from, uint256 time) private {
         categories[from].push(time);
     }
-    
+
     function getList()
     public
     view
@@ -17,14 +23,23 @@ contract SimpleStore {
     {
         return categories[msg.sender];
     }
-    
-    function add(string text, uint256 time) public {
-        records[msg.sender][time]=text;
+
+    function add(string text, uint256 time, string msgType) public {
+        Message memory message;
+
+        if(keccak256(msgType) == keccak256("image")) {
+            message = Message("image", text);
+        } else {
+            message = Message("text", text);
+        }
+
+        records[msg.sender][time]=message;
         _addToList(msg.sender, time);
-        emit Recorded(msg.sender, text, time);
+        emit Recorded(msg.sender, text, msgType, time);
     }
-    function get(uint256 time) public view returns(string) {
-        
-        return records[msg.sender][time];
+
+    function get(uint256 time) public view returns(string, string) {
+        Message message = records[msg.sender][time];
+        return (message.msgType, message.msgContent);
     }
 }
