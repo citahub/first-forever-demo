9641c51c20168a2dc685a18755098904b676935c
travis_joe <qiuzengyuan@rivtower.com>
2020-03-21 10:46:14 +0800

    feat: upgrade script add contract name

--- src/contracts/upgrade.js
+++ src/contracts/upgrade.js
@@ -4,7 +4,8 @@ const path = require("path");
 const location = process.argv.slice(2)[0];
 const {
   contractAddress: upgradeAddress,
-  abi: upgradeAbi
+  abi: upgradeAbi,
+  contractName,
 } = require(path.resolve(location));
 // const from = cita.base.accounts.wallet[0].address;
 const {
@@ -20,7 +21,7 @@ cita.base
     transaction.validUntilBlock = height + 80;
   })
   .then(() => {
-    UpdateManagerContract.methods.upgradeTo(upgradeAddress).send(transaction).then(hash => console.log(hash))
+    UpdateManagerContract.methods.upgradeTo(upgradeAddress, contractName).send(transaction).then(hash => console.log(hash))
   })
   .then(() => {
     cita.base.storeAbi(contractAddress, upgradeAbi, transaction).then(console.log);
