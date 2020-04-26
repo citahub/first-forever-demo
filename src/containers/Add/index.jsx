16e4463e46df63aa8811d75567b47352230ddf3b
travis_joe <qiuzengyuan@rivtower.com>
2020-04-26 16:20:31 +0800

    feat:  add links to microscope
    - .env add REACT_APP_MICROSCOPE for the microscope link

--- src/containers/Add/index.jsx
+++ src/containers/Add/index.jsx
@@ -9,6 +9,11 @@ import {
 import cita from '../../cita-sdk'
 
 const timeFormatter = time => ('' + time).padStart(2, '0')
+const {
+  REACT_APP_RUNTIME,
+  REACT_APP_MICROSCOPE,
+} = process.env
+
 const submitTexts = {
   normal: '愿此刻永恒',
   submitting: '保存中',
@@ -21,6 +26,7 @@ class Add extends React.Component {
     time: new Date(),
     submitText: submitTexts.normal,
     errorText: '',
+    hash: '',
   }
   handleInput = e => {
     this.setState({
@@ -33,65 +39,81 @@ class Add extends React.Component {
       text
     } = this.state
     cita.base
-      .getBlockNumber()
-      .then(current => {
-        const tx = {
-          ...transaction,
-          validUntilBlock: +current + 88,
-        }
-        tx.from = cita.getFromAddress()
-        this.setState({
-          submitText: submitTexts.submitting,
-        })
-        return simpleStoreContract.methods.add(text, +time).send(tx)
+    .getBlockNumber()
+    .then(current => {
+      const tx = {
+        ...transaction,
+        validUntilBlock: +current + 88,
+      }
+      tx.from =
+          cita.getFromAddress()
+      this.setState({
+        submitText: submitTexts.submitting,
       })
-      .then(res => {
-        if (res.hash) {
-          return cita.listeners.listenToTransactionReceipt(res.hash).then(receipt => {
-            if (!receipt.errorMessage) {
-              this.setState({
-                submitText: submitTexts.submitted
-              })
-            } else {
+      return simpleStoreContract.methods.add(text, +time).send(tx)
+    })
+    .then(res => {
+      if (res.hash) {
+        this.setState({hash: res.hash})
+        return cita.listeners.listenToTransactionReceipt(res.hash).then(
+            receipt => {
+              if (!receipt.errorMessage) {
+                this.setState({
+                  submitText: submitTexts.submitted
+                })
+              } else {
                 alert(receipt.errorMessage)
                 this.props.history.push('/');
-            }
-          })
-        } else {
-            alert("Transaction send failed")
-            this.props.history.push('/');
-        }
-      })
+              }
+            })
+      } else {
+        alert("Transaction send failed")
+        this.props.history.push('/');
+      }
+    })
   }
+
   render() {
-    const { time, text, submitText, errorText } = this.state
+    const {time, text, submitText, errorText} = this.state
     return (
-      <div className="add__content--container">
-        <div className="add__time--container">
-          <span className="add__time--year"> {time.getFullYear()} </span>:{' '}
-          <span className="add__time--month"> {timeFormatter((time.getMonth() + 1) % 12)} </span>:{' '}
-          <span className="add__time--day"> {timeFormatter(time.getDate())} </span>:{' '}
-          <span className="add__time--hour"> {timeFormatter(time.getHours())} </span>:{' '}
-          <span className="add__time--min"> {timeFormatter(time.getMinutes())} </span>{' '}
+        <div className="add__content--container">
+          <div className="add__time--container">
+            <span
+                className="add__time--year"> {time.getFullYear()} </span>:{' '}
+            <span className="add__time--month"> {timeFormatter(
+                (time.getMonth() + 1) % 12)} </span>:{' '}
+            <span className="add__time--day"> {timeFormatter(
+                time.getDate())} </span>:{' '}
+            <span className="add__time--hour"> {timeFormatter(
+                time.getHours())} </span>:{' '}
+            <span className="add__time--min"> {timeFormatter(
+                time.getMinutes())} </span>{' '}
+          </div>
+          <div className="add__content--prompt">
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref="#icon-icon-time"/>
+            </svg>
+            {' '}
+            <span> 把你觉得重要的一刻， 存放在链上， 永远保存， 随时查看 </span>{' '}
+          </div>
+          {' '}
+          <textarea
+              cols="32"
+              rows="10"
+              className="add__content--textarea"
+              placeholder="留下你的时光吧..."
+              onChange={this.handleInput}
+              value={text}
+          />{' '}
+          <Submit text={submitText} onClick={this.handleSubmit}
+                  disabled={submitText !== submitTexts.normal}/>{' '}
+          {this.state.hash && <a className="toMicroscope" href={`${REACT_APP_MICROSCOPE}/#/transaction/hash/${this.state.hash}`}>查询交易Hash</a>}
+          {errorText && <span className="warning"> {errorText} </span>}
+          <BottomNav showAdd={false}/>{' '}
+
         </div>
-        <div className="add__content--prompt">
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-icon-time" />
-          </svg>{' '}
-          <span> 把你觉得重要的一刻， 存放在链上， 永远保存， 随时查看 </span>{' '}
-        </div>{' '}
-        <textarea
-          cols="32"
-          rows="10"
-          className="add__content--textarea"
-          placeholder="留下你的时光吧..."
-          onChange={this.handleInput}
-          value={text}
-        />{' '}
-        <Submit text={submitText} onClick={this.handleSubmit} disabled={submitText !== submitTexts.normal} />{' '}
-        {errorText && <span className="warning"> {errorText} </span>} <BottomNav showAdd={false} />{' '}
-      </div>
     )
   }
 }
+
 export default Add
